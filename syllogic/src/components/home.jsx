5ee5e9171a4e24fea532bb087b86d47ad1bcbb75
iniyan <iniyansg93@gmail.com>
2023-02-03 20:25:41 +0530

    button-lets go

--- syllogic/src/components/home.jsx
+++ syllogic/src/components/home.jsx
@@ -3,22 +3,29 @@ export default function Home() {
 
     return (
         <>
+            <div class="full1">
+               
+                <div class="dept1">
+                    <h5 class="dept">Chemical department proudly present's</h5>
 
-        <div class="top">
+                </div>
 
-            <h1 class="dept">kongu engineering college</h1>
 
-        </div>
-        <div class="dept1"> 
-        <h2 class="dept">Chemical department proudly present's</h2>
 
-        </div>
-       
-           
-        
-            <div class="wrapper">
-                <div class="bg"> Syllogic 2k23 </div>
-                <div class="fg">Syllogic 2k23 </div>
+                <div class="wrapper">
+                    <div class="bg"> Syllogic 2k23 </div>
+                    <div class="fg">Syllogic 2k23 </div>
+                </div>
+
+                
+                <div class="wrap">
+                    <button class="button">Let's Go</button>
+                </div>
+            </div>
+
+
+            <div>
+                college details
             </div>
         </>
     )
