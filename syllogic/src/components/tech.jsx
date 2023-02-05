7d8f45ea18b46dd7e0bf7608d65a97f3eb94097f
Bharath <95229845+BHARATH2503@users.noreply.github.com>
2023-02-06 01:12:58 +0530

    Update tech.jsx

--- syllogic/src/components/tech.jsx
+++ syllogic/src/components/tech.jsx
@@ -25,14 +25,6 @@ export default function Tech() {
                         </Link>
                     </p>
                 </Navbar>
-                    <Navbar expand="lg" fixed="top" >
-                        <p className="navbrand">
-                            <Link to="/">
-                                <Navbar.Brand className="navbrand1">SYLLOGIC 2K23</Navbar.Brand>
-                                <img src={Logo} alt="Logo" className="logo1" />
-                            </Link>
-                        </p>
-                    </Navbar>
                 </div>
             <h1 id="pageHeaderTitle" className="techheader">Technical events</h1>
             <div className='div1'>
@@ -74,48 +66,6 @@ export default function Tech() {
                     </li>
                 </ul>
             </div>
-            <div className="tech">
-                <h1 id="pageHeaderTitle" className="techheader">Technical events</h1>
-                <div className='div1'>
-                    <ul class="cards">
-                        <li style={{ marginLeft: "50px" }}>
-                            <div class="card">
-                                <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-                                <div class="card__overlay">
-                                    <div class="card__header">
-                                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                                        {/* <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" /> */}
-                                        <div class="card__header-text">
-                                            <h3 class="card__title">Paper Presentation</h3>
-
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <button className="btn" id="but1">Register</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li style={{ marginLeft: "50px" }}>
-                            <div class="card">
-                                <img src="https://i.imgur.com/oYiTqum.jpg" class="card__image" alt="" />
-                                <div class="card__overlay">
-                                    <div class="card__header">
-                                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                                        <div class="card__header-text">
-                                            <h3 class="card__title">Poster</h3>
-
-                                    </div>
-                                </div>
-                                <div>
-                                    <Link to={`/poster`}><button className="btn" id="d1">Details</button></Link>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        
+            </>  
     )
 }
